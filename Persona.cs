6f3dee298aff2e5accb00cd9b91227734ec9785f
Belkis Vaq <55562967+BelkisVasquez0609@users.noreply.github.com>
2023-01-05 20:27:50 -0400

    Clase persona

---
+++ Persona.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoEstudiantes
+{
+    public class Persona
+    {
+        string Nombres;
+        string Apellidos;
+        int Edad; 
+    }
+}
